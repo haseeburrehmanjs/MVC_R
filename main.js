c5ed65467eaa79bc3f4ff1865e2ad375626cd42b
haseeburrehmanjs <kamranking786500@gmail.com>
2025-03-18 00:21:06 +0500

    install cors

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { inquiryRoutes } from './app/routes/inquiry.routes.js'
 
 dotenv.config()
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/website/api', inquiryRoutes)
 
